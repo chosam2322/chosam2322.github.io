c126d674445fc870ad27d79149a8832fc862502c
Christian Hosam <47082889+chosam2322@users.noreply.github.com>
2023-08-12 14:40:14 -0700

    Update zoom.js

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,10 +1,6 @@
 // Initialize medium zoom.
 $(document).ready(function() {
   medium_zoom = mediumZoom('[data-zoomable]', {
-<<<<<<< HEAD
-    margin: 100,
-=======
->>>>>>> origin/master
     background: getComputedStyle(document.documentElement)
         .getPropertyValue('--global-bg-color') + 'ee',  // + 'ee' for trasparency.
   })
